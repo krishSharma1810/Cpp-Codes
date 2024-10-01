e975fe766a25e12d32171828fe6deb1976e30f4e
Krish <sharmakrish1810work@gmail.com>
2024-10-01 16:57:48 +0530

    Initial commit

---
+++ Workers_problem.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<map>
+#include <utility> // for std::pair
+using namespace std;
+
+
+
+class Maps{
+    private:
+    int Worker,Task,i,a,b,c;
+    map<char,pair<char,int>> MainMap;
+    public:
+    void Take();
+    Void Logic();
+    void Print();
+};
+
+
+void Maps :: Take(){
+    cin >> Worker >> Task ;
+    for (i=0;i<Task;i++){
+
+    }
+
+}
+
+
+int main(){
+
+return 0;
+
+}
+
+
+
---
+++ calculator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class calculation{
+  private:
+    int n;
+};
+
+
+int main()
+{
+  int ans;
+  cin >> ans;
+  char oper; 
+}
